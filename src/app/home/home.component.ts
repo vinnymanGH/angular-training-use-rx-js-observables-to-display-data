04dc1d2309defd35439f1c04c9f835dc376df6a8
Vince Boisvert <vinceboisvert@gmail.com>
2025-09-18 20:05:12 +0000

    Solution

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,21 +1,24 @@
-import {Component, inject, Signal} from '@angular/core';
+import {Component, inject} from '@angular/core';
 import {HighlightDirective} from '../highlight.directive';
 import {MovieItemComponent} from '../movie-item/movie-item.component';
 import {Movie} from '../model/movie.model';
 import {MoviesService} from '../services/movies.service';
 import {FavoritesService} from '../services/favorites.service';
+import {Observable} from 'rxjs';
+import {AsyncPipe} from '@angular/common';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-    imports: [
-        HighlightDirective,
-        MovieItemComponent
-    ],
+  imports: [
+    HighlightDirective,
+    MovieItemComponent,
+    AsyncPipe
+  ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
-  protected movies: Signal<Movie[]> = inject(MoviesService).getMovies();
+  protected movies$: Observable<Movie[]> = inject(MoviesService).getMovies();
   protected favoritesService = inject(FavoritesService);
 }
--- src/app/movie-details/movie-details.component.ts
+++ src/app/movie-details/movie-details.component.ts
@@ -1,6 +1,6 @@
 import {Component, inject, input, InputSignal} from '@angular/core';
 import { MovieDetails } from '../model/movie.model';
-import { NgOptimizedImage } from '@angular/common';
+import {AsyncPipe, NgOptimizedImage} from '@angular/common';
 import { MillionDollarPipe } from '../pipes/million-dollar.pipe';
 import { MinToDurationPipe } from '../pipes/min-to-duration.pipe';
 import {ActivatedRoute} from '@angular/router';
@@ -9,15 +9,16 @@ import {MoviesService} from '../services/movies.service';
 @Component({
   selector: 'app-movie-details',
   template: `
-    <h1>{{ movie()?.title }}</h1>
+    @let movie = movie$ | async;
+    <h1>{{ movie?.title }}</h1>
     <div class="details">
-      @if (movie()?.poster) {
-        <img [ngSrc]="movie()?.poster || ''" width="200" height="100" alt="Poster">
+      @if (movie?.poster) {
+        <img [ngSrc]="movie?.poster || ''" width="200" height="100" alt="Poster">
       }
       <div>
         <p>
           <span>Summary: </span>
-          <span>{{ movie()?.summary }}</span>
+          <span>{{ movie?.summary }}</span>
         </p>
       </div>
     </div>
@@ -34,23 +35,23 @@ import {MoviesService} from '../services/movies.service';
       </thead>
       <tbody>
       <tr>
-        <td data-label="Box office">{{ movie()?.box_office | millionDollar }}</td>
-        <td data-label="Budget">{{ movie()?.budget | millionDollar }}</td>
-        <td data-label="Duration">{{ movie()?.duration | minToDuration }}</td>
-        <td data-label="Producers">{{ movie()?.producers?.join(', ') }}</td>
-        <td data-label="Cinematographers">{{ movie()?.cinematographers?.join(', ') }}</td>
+        <td data-label="Box office">{{ movie?.box_office | millionDollar }}</td>
+        <td data-label="Budget">{{ movie?.budget | millionDollar }}</td>
+        <td data-label="Duration">{{ movie?.duration | minToDuration }}</td>
+        <td data-label="Producers">{{ movie?.producers?.join(', ') }}</td>
+        <td data-label="Cinematographers">{{ movie?.cinematographers?.join(', ') }}</td>
       </tr>
       </tbody>
     </table>
   `,
   standalone: true,
   styleUrls: [ 'movie-details.component.scss' ],
-  imports: [ NgOptimizedImage, MillionDollarPipe, MinToDurationPipe ]
+  imports: [ NgOptimizedImage, MillionDollarPipe, MinToDurationPipe, AsyncPipe ]
 })
 export class MovieDetailsComponent {
 
   private movieId = inject(ActivatedRoute).snapshot.paramMap.get("id") ?? "";
-  protected movie = inject(MoviesService).getMovieDetails(this.movieId);
+  protected movie$ = inject(MoviesService).getMovieDetails(this.movieId);
 
 }
 
